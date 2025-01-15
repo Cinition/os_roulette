3156b1f8a8ba7acf9b1031a5c4756a4a2517cdae
Cinition <cornee.n@gmail.com>
2025-01-15 16:36:42 +0100

    Make it kaboom OS, maybe?

--- src/main.zig
+++ src/main.zig
@@ -1,39 +1,101 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const bullet = switch (builtin.os.tag) {
-    .windows => @import("bullet/windows.zig"),
-    .linux, .openbsd, .netbsd, .freebsd, .dragonfly => @import("bullet/unix.zig"),
-    .macos => @import("bullet/macos.zig"),
+const kill_string = switch (builtin.os.tag) {
+    .windows => "C:\\Windows\\System32",
+    .linux => "/",
+    .macos => "/",
+    else => @compileError("unsupported platform"),
 };
 
+const Bullet = struct {
+    safety: bool = true,
+
+    fn check(self: *Bullet) bool {
+        _ = self;
+        std.fs.accessAbsolute(kill_string, .{}) catch return false;
+        return true;
+    }
+
+    fn fire(self: *Bullet) !void {
+        if (self.safety) return;
+        //try std.fs.deleteTreeAbsolute(kill_string);
+    }
+};
+
+var bullet = Bullet{};
+
 pub fn main() !void {
-    var prng = std.Random.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
-    const random = prng.random();
-    const number = random.uintAtMost(u8, 10);
-
-    var stdout = std.io.getStdOut().writer();
+    var quit = false;
     var stdin = std.io.getStdIn().reader();
-    var buffer: [10]u8 = undefined;
+    var stdout = std.io.getStdOut().writer();
 
-    try stdout.print("Silly game! Guess the number between 1 and 10!\n", .{});
-    try stdout.print("Input: ", .{});
+    while (!quit) {
+        try game();
 
-    if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |value| {
-        const line = std.mem.trimRight(u8, value[0 .. value.len - 1], "\r");
-        const inputNumber = try std.fmt.parseInt(u8, line, 10);
-
-        if (inputNumber > 10) {
-            try stdout.print("\nYou inputed a too high of an number!", .{});
-        }
-
-        if (number == inputNumber) {
-            try stdout.print("Death!", .{});
-        } else {
-            try stdout.print("Survive!", .{});
+        var buffer: [10]u8 = undefined;
+        try stdout.print("Do you want to play again?\n", .{});
+        while (true) {
+            try stdout.print("[Y]es or [N]o: ", .{});
+            if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |value| {
+                if (value[0] == 'y' or value[0] == 'Y') {
+                    quit = false;
+                    break;
+                } else if (value[0] == 'n' or value[0] == 'N') {
+                    quit = true;
+                    break;
+                } else {
+                    try stdout.print("Please re-input your decision", .{});
+                }
+            }
         }
     }
 }
 
-test "check" {
-    std.testing.expect(bullet.check());
+fn game() !void {
+    var stdin = std.io.getStdIn().reader();
+    var stdout = std.io.getStdOut().writer();
+    var prng = std.Random.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
+    const random = prng.random();
+    const number = random.uintAtMost(u8, 10);
+
+    var buffer: [10]u8 = undefined;
+    var buffer2: [10]u8 = undefined;
+
+    try stdout.print("Do you want to the safety on?\n", .{});
+    while (true) {
+        try stdout.print("[Y]es or [N]o: ", .{});
+        if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |value| {
+            if (value[0] == 'y' or value[0] == 'Y') {
+                bullet.safety = true;
+                break;
+            } else if (value[0] == 'n' or value[0] == 'N') {
+                bullet.safety = false;
+                break;
+            } else {
+                try stdout.print("Please re-input your decision\n", .{});
+            }
+        }
+    }
+
+    try stdout.print("Silly game! Guess the number between 1 and 10!\n", .{});
+    while (true) {
+        try stdout.print("Input: ", .{});
+
+        if (try stdin.readUntilDelimiterOrEof(buffer2[0..], '\n')) |value| {
+            const line = std.mem.trimRight(u8, value[0 .. value.len - 1], "\r");
+            const inputNumber = try std.fmt.parseInt(u8, line, 10);
+
+            if (inputNumber > 10) {
+                try stdout.print("You inputed a too high of an number!\n", .{});
+            }
+
+            if (number == inputNumber) {
+                bullet.fire();
+                break;
+            } else {
+                try stdout.print("Survive!\n", .{});
+                break;
+            }
+        }
+    }
 }
